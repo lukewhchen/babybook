cebd6982162989ddd78cc66be000fc9cb821268f
lukewhchen <lukewhchen@gmail.com>
2018-05-08 14:39:41 -0400

    npm install & webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/baby_book.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", "*" ]
+  },
+  devtool: 'source-map',
+};
