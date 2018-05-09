29592b1b7effb96671e1b9640c4aa42e96fc88ac
lukewhchen <lukewhchen@gmail.com>
2018-05-09 12:37:41 -0400

    finish session util & actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,48 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = (user) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    user: user
+  };
+};
+
+export const logoutCurrentUser = () => {
+  return {
+    type: LOGOUT_CURRENT_USER
+  };
+};
+
+export const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_ERRORS,
+    errors
+  };
+};
+
+
+export const login = user => dispatch => (
+  SessionAPIUtil.login(user).then( user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const logout = () => dispatch => (
+  SessionAPIUtil.logout().then(() => (
+    dispatch(logoutCurrentUser())
+  ))
+);
+
+export const signup = user => dispatch => (
+  SessionAPIUtil.signup(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
