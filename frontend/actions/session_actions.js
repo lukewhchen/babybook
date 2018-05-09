3593770194dc4a0920946e40fc046d7b57654976
lukewhchen <lukewhchen@gmail.com>
2018-05-09 14:01:14 -0400

    finish to rootReducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -4,10 +4,10 @@ export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
 
-export const receiveCurrentUser = (user) => {
+export const receiveCurrentUser = (currentUser) => {
   return {
     type: RECEIVE_CURRENT_USER,
-    user: user
+    user: currentUser
   };
 };
 
@@ -24,6 +24,13 @@ export const receiveErrors = (errors) => {
   };
 };
 
+export const signup = user => dispatch => (
+  SessionAPIUtil.signup(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
 
 export const login = user => dispatch => (
   SessionAPIUtil.login(user).then( user => (
@@ -36,13 +43,5 @@ export const login = user => dispatch => (
 export const logout = () => dispatch => (
   SessionAPIUtil.logout().then(() => (
     dispatch(logoutCurrentUser())
-  ))
-);
-
-export const signup = user => dispatch => (
-  SessionAPIUtil.signup(user).then(user => (
-    dispatch(receiveCurrentUser(user))
-  ), err => (
-    dispatch(receiveErrors(err.responseJSON))
   ))
 );
