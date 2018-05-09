3e85b5d13ca2e63cf552d9249f3edb92360db573
lukewhchen <lukewhchen@gmail.com>
2018-05-09 14:28:00 -0400

    begin test root component

--- frontend/baby_book.jsx
+++ frontend/baby_book.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  
   ReactDOM.render(<h1>Welcome to the BabyBook</h1>, root);
 });
---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Test from app.jsx</h1>
+  </div>
+);
+
+export default App;
