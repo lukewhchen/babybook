dd3731e90b0a9471ad77a97797fcb937e9755f28
lukewhchen <lukewhchen@gmail.com>
2018-05-09 17:20:33 -0400

    bootstrapping work

--- frontend/baby_book.jsx
+++ frontend/baby_book.jsx
@@ -5,7 +5,19 @@ import Root from './components/root';
 import * as SessionAPIUtil from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser}
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
 
   window.getState = store.getState;
