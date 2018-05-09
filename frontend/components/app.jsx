d61388d3ba97d4f189d1061ca18beae349a7f183
lukewhchen <lukewhchen@gmail.com>
2018-05-09 17:55:39 -0400

    finish auth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
+
 import GreetingContainer from './greeting/greeting_container';
-import { Route } from 'react-router-dom';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
     <header>
-      <h1>Welcome to BabyBook</h1>
+      <Link to="/">
+        <h1>Welcome to BabyBook</h1>
+      </Link>
       <GreetingContainer />
     </header>
-
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <Switch>
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>
   </div>
 );
 
