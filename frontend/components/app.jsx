74ecdd12189d8eeaf737a96f021063f570865123
lukewhchen <lukewhchen@gmail.com>
2018-05-10 11:36:09 -0400

    create signup form

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,6 +5,7 @@ import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import ASignupFormContainer from './session_form/another_signup_form_container';
 
 const App = () => (
   <div>
@@ -12,21 +13,26 @@ const App = () => (
         <h1>babybook</h1>
       <GreetingContainer />
     </header>
+
     <nav id='bb-login'>
       <Switch>
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
       </Switch>
     </nav>
+
     <div id='left-side-login'>
       <h2>left side</h2>
     </div>
+
     <div id='right-side-login'>
-      <h2>Sign up form</h2>
+      <ASignupFormContainer />
     </div>
+
     <div id='footer'>
       <p>babybook @ 2018 developed by Luke Chen</p>
     </div>
+
   </div>
 );
 
---
+++ frontend/components/session_form/another_signup_form_container.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { signup } from '../../actions/session_actions';
+import { Link } from 'react-router-dom';
+
+class ASignupForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { first_name: "", last_name: "", email: "", password: "" };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.createUserObject = this.createUserObject.bind(this);
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    const user = this.createUserObject();
+    this.props.processForm(user);
+  }
+
+  createUserObject(){
+    const first_name = this.first_name.value;
+    const last_name = this.last_name.value;
+    const email = this.email.value;
+    const password = this.password.value;
+    return { first_name, last_name, email, password };
+  }
+
+  render(){
+    return(
+      <div id="another_signup_form_container">
+        <h2>Sign Up</h2>
+        <h4>It's free and always will be.</h4>
+        <form onSubmit={this.handleSubmit}>
+          <span>
+            <input type='text'
+                  placeholder='First name'
+                  ref={(input) => this.first_name = input}></input>
+            <input type='text'
+                  placeholder='Last name'
+                  ref={(input) => this.last_name = input}></input>
+          </span>
+
+          <input type='text'
+                placeholder='Email address'
+                ref={(input) => this.email = input}></input>
+          <input type='password'
+                placeholder='Password'
+                ref={(input) => this.password = input}></input>
+
+          <div>
+            <select defaultValue='gender'
+                    ref={(input) => this.gender = input}>
+              <option disabled value='gender'>Gender</option>
+              <option value='male'>Male</option>
+              <option value='female'>Female</option>
+            </select>
+          </div>
+
+          <button id='signup-button'>Create Account</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ errors }) => {
+  return {
+    errors: errors.session,
+    fromType: 'signup',
+    navLink: <Link to="/login">Log In Here</Link>
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return ({
+    processForm: (user) => dispatch(signup(user))
+  });
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ASignupForm);
