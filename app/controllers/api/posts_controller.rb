cfbcd8e425775d030eff1f39b64709619c466b02
lukewhchen <lukewhchen@gmail.com>
2018-05-10 23:03:02 -0400

    finish posts controller

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,43 @@
+class Api::PostsController < ApplicationController
+  before_action :ensure_logged_in
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      @post.author = current_user
+      render :post
+    else
+      render json: @post.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      render :post
+    else
+      render json: @post.errors.full_messages, status: 422
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    render :post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :post_image_url)
+  end
+
+end
