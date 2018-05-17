3aa40f3044d185c48fffcbb413cdd1086a6e5a7a
lukewhchen <lukewhchen@gmail.com>
2018-05-17 15:05:36 -0400

    add left-icon

--- frontend/components/post/left_sidebar.jsx
+++ frontend/components/post/left_sidebar.jsx
@@ -1,47 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+      // <h3>Personal information</h3>
 const LeftSidebar = () => {
   return (
-    <div id="left-side">
-      <h3>Personal information</h3>
-      <Link to='/'>
-        <i className="fa fa-newspaper-o" aria-hidden="true"></i>
-        News Feed
-      </Link>
-      <li>
-        <i className="fa fa-commenting-o" aria-hidden="true" />
-        Comment
-      </li>
-      <li>
-        <i className="fa fa-thumbs-o-up" aria-hidden="true" />
-        Like
-      </li>
-      <li>
-        <i className="fa fa-share" aria-hidden="true" />
-        Share
-      </li>
-      <li>
-        <i className="fa fa-search" aria-hidden="true" />
-        Search
-      </li>
-      <li>
-        <i className="fa fa-user-circle" aria-hidden="true" />
-        User
-      </li>
-      <li>
-        <i className="fa fa-flash" aria-hidden="true" />
-      </li>
-      <li>
-        <i className="fa fa-child" aria-hidden="true" />
-      </li>
-      <li>
-        <i className="fa fa-ellipsis-h" aria-hidden="true" />
-      </li>
-
-
-    </div>
+    <li className="left-side">
+      <label className="left-icon">
+        <i className="fa fa-user-circle" aria-hidden="true">User</i><br/>
+        <i className="fa fa-newspaper-o" aria-hidden="true">News Feed</i><br/>
+        <i className="fa fa-commenting-o" aria-hidden="true">Messenger</i><br/>
+        <i className="fa fa-tv" aria-hidden="true">Watch</i><br/>
+        <i className="fa fa-calendar" aria-hidden="true">Events</i><br/>
+        <i className="fa fa-gamepad" aria-hidden="true">Games</i><br/>
+      </label>
+    </li>
   );
 };
 
 export default LeftSidebar;
+
+
+// <i className="fa fa-search" aria-hidden="true">Search</i>
+// <i className="fa fa-flash" aria-hidden="true" />
+// <i className="fa fa-child" aria-hidden="true" />
+// <i className="fa fa-ellipsis-h" aria-hidden="true" />
