f6c864f0c2070d800cd3c33a5c9da3a80d855733
lukewhchen <lukewhchen@gmail.com>
2018-06-05 17:55:36 -0400

    modify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,26 @@
 
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
+
+let plugins = [];
+let devPlugins = [];
+
+let prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
   context: __dirname,
