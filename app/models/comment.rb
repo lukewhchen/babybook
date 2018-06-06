bb72791e265212070cdb4a292fee5767a2aa43eb
lukewhchen <lukewhchen@gmail.com>
2018-06-06 18:47:02 -0400

    fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :body, :author_id, :post_id presence: true
+  validates :body, :author_id, :post_id, presence: true
 
   belongs_to :author,
     foreign_key: :author_id,
