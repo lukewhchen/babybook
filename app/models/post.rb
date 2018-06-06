80ae1778d2b73a45ce37116ca6b5ef284fc11087
lukewhchen <lukewhchen@gmail.com>
2018-06-06 09:46:09 -0400

    add comment assocation

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,8 @@ class Post < ApplicationRecord
     foreign_key: :author_id,
     class_name: :User
 
+  has_many :comments,
+    foreign_key: :post_id,
+    class_name: :Comment
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,10 @@ class User < ApplicationRecord
   has_many :posts,
     foreign_key: :author_id,
     class_name: :Post
+    
+  has_many :comments,
+    foreign_key: :author_id,
+    class_name: :Comment
 
   attr_reader :password
 
