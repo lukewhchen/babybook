6bd0ea33ce7aa51d450ff2569f3501685951d6d9
lukewhchen <lukewhchen@gmail.com>
2018-06-05 22:41:01 -0400

    add comment route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :show, :create, :update]
     resource :session, only: [:create, :destroy]
     resources :posts, only: [:index, :show, :create, :update, :destory]
+    resources :comments, only: [:index, :show, :create, :update, :destroy]
   end
 end
