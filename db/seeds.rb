26f34016e216d605a854757625c05485e56b7630
lukewhchen <lukewhchen@gmail.com>
2018-06-06 16:58:06 -0400

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ DefaultPhoto = [
   imgs = []
 DefaultPhoto.each do |el|
   # imgs << File.open(File.join(Rails.root, 'assets', 'images', el))
-  imgs << File.open(File.join(Rails.root, "app/assets/images/#{el}"))
+  imgs << File.open(File.join(Rails.root, "assets/images/#{el}"))
   # imgs << File.open(asset_path(el))
 end
 # img = File.open(File.join(Rails.root, 'app', 'assets', 'images', 'cutebaby.jpg'))
