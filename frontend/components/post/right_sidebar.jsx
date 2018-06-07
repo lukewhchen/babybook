b224d0698dcf5ffef86836aa9148283038cc1ef7
lukewhchen <lukewhchen@gmail.com>
2018-06-07 14:34:05 -0400

    fix user name

--- frontend/components/post/right_sidebar.jsx
+++ frontend/components/post/right_sidebar.jsx
@@ -5,14 +5,14 @@ const RightSidebar = () => {
     <li className="right-side">
       <h4>Best Things to Do With Kids in NYC This Weekend</h4>
         <i className="fa fa-child" aria-hidden="true" /><a href="https://mommypoppins.com/new-york-city-kids/parks-playgrounds/new-york-citys-best-playgrounds-slides-swings-worthy-of-a-subway" target="_blank">NYC's Best Playgrounds</a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.parents.com/fun/activities/outdoor/weekend-family-activities/" target="_blank">20 Activities to Do With Your Family This Weekend</a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="http://www.playbill.com/article/see-anna-elsa-and-the-cast-of-frozen-on-broadway-come-to-life-in-new-portraits" target="_blank">See Anna, Elsa, and the Cast of Frozen on Broadway </a><br/>
+        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.parents.com/fun/activities/outdoor/weekend-family-activities/" target="_blank">20 Activities to Do With Your Family &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This Weekend</a><br/>
+        <i className="fa fa-child" aria-hidden="true" /><a href="http://www.playbill.com/article/see-anna-elsa-and-the-cast-of-frozen-on-broadway-come-to-life-in-new-portraits" target="_blank">See Anna, Elsa, and the Cast of &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Frozen on Broadway </a><br/>
         <i className="fa fa-child" aria-hidden="true" /><a href="https://www.timeout.com/new-york-kids/museums/nyc-museums-best-exhibits-for-kids" target="_blank">The best museums for kids in NYC</a><br/>
         <i className="fa fa-child" aria-hidden="true" /><a href="https://www.timeout.com/new-york-kids/shopping/baby-stores-in-nyc" target="_blank">The best baby stores in NYC</a><br/>
         <i className="fa fa-child" aria-hidden="true" /><a href="https://www.ratemds.com/best-doctors/ny/new-york/pediatrician/" target="_blank">Best Pediatricians in New York</a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.todaysparent.com/checklists/newborn-checklist/" target="_blank">Newborn checklist: Everything you need before your baby arrives</a><br/>
+        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.todaysparent.com/checklists/newborn-checklist/" target="_blank">Newborn checklist: Everything you &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;need before your baby arrives</a><br/>
         <i className="fa fa-child" aria-hidden="true" /><a href="https://www.pampers.com/en-us/pregnancy/baby-shower/article/baby-shower-games" target="_blank">11 Types of Fun Baby Shower Games</a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.independent.co.uk/news/business/news/peppa-pig-entertainment-one-117-new-episodes-cartoon-global-market-a7743991.html" target="_blank">Peppa Pig owner Entertainment One announces 117 new episodes</a><br/>
+        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.independent.co.uk/news/business/news/peppa-pig-entertainment-one-117-new-episodes-cartoon-global-market-a7743991.html" target="_blank">Peppa Pig owner Entertainment One &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;announces 117 new episodes</a><br/>
     </li>
   );
 };
