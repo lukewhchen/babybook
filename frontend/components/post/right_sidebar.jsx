ee6f1c5f5483efe35ab233ad3ed255062cd7f1ca
lukewhchen <lukewhchen@gmail.com>
2018-09-13 10:40:29 -0400

    fix font

--- frontend/components/post/right_sidebar.jsx
+++ frontend/components/post/right_sidebar.jsx
@@ -3,16 +3,25 @@ import React from 'react';
 const RightSidebar = () => {
   return (
     <li className="right-side">
-      <h4>Best Things to Do With Kids in NYC This Weekend</h4>
-        <i className="fa fa-child" aria-hidden="true" /><a href="https://mommypoppins.com/new-york-city-kids/parks-playgrounds/new-york-citys-best-playgrounds-slides-swings-worthy-of-a-subway" target="_blank">NYC's Best Playgrounds</a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.parents.com/fun/activities/outdoor/weekend-family-activities/" target="_blank">20 Activities to Do With Your Family &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This Weekend</a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="http://www.playbill.com/article/see-anna-elsa-and-the-cast-of-frozen-on-broadway-come-to-life-in-new-portraits" target="_blank">See Anna, Elsa, and the Cast of &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Frozen on Broadway </a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.timeout.com/new-york-kids/museums/nyc-museums-best-exhibits-for-kids" target="_blank">The best museums for kids in NYC</a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.timeout.com/new-york-kids/shopping/baby-stores-in-nyc" target="_blank">The best baby stores in NYC</a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.ratemds.com/best-doctors/ny/new-york/pediatrician/" target="_blank">Best Pediatricians in New York</a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.todaysparent.com/checklists/newborn-checklist/" target="_blank">Newborn checklist: Everything you &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;need before your baby arrives</a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.pampers.com/en-us/pregnancy/baby-shower/article/baby-shower-games" target="_blank">11 Types of Fun Baby Shower Games</a><br/>
-        <i className="fa fa-child" aria-hidden="true" /><a href="https://www.independent.co.uk/news/business/news/peppa-pig-entertainment-one-117-new-episodes-cartoon-global-market-a7743991.html" target="_blank">Peppa Pig owner Entertainment One &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;announces 117 new episodes</a><br/>
+      <h4>Best Things to Do With Kids in the Bay Area This Weekend</h4>
+        <i className="fa fa-child" aria-hidden="true" />
+        <a href="http://www.marinemammalcenter.org/" target="_blank">Meet the marine mammals in Sausalito</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+        <a href="https://www.mlb.com/giants/ballpark/attractions" target="_blank">Root for the home team</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+        <a href="https://goldengatepark.com/" target="_blank">Spend some time in Golden Gate Park</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+        <a href="http://www.ebparks.org/parks/tilden/" target="_blank">Take the tots to Tilden</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+        <a href="https://www.cityofberkeley.info/adventureplayground/" target="_blank">Up your adventure in Berkeley</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+        <a href="https://www.spn.usace.army.mil/missions/recreation/bay-model-visitor-center/" target="_blank">Explore the Bay Model</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+        <a href="http://redtri.com/san-francisco/things-to-do-in-the-presidio/slide/1" target="_blank">Play in the Presidio</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+        <a href="http://redtri.com/san-francisco/best-san-francisco-playgrounds/" target="_blank">Check out a new playground</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+        <a href="https://www.independent.co.uk/news/business/news/peppa-pig-entertainment-one-117-new-episodes-cartoon-global-market-a7743991.html" target="_blank">Peppa Pig owner Entertainment One &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;announces 117 new episodes</a><br/>
     </li>
   );
 };
