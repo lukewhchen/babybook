94268207bdf0f266b569ef120b1480179efb1892
lukewhchen <lukewhchen@gmail.com>
2018-09-17 15:42:11 -0400

    update aws

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
   validates :body, :author_id, presence: true
   # DefaultPhoto = ["big-girl.jpg", "cover-1.jpg", "emma.jpg"]
-  DPhoto = ["youcan.jpg", "hithere.jpg", "cutebaby.jpg"]
+  Default = ["youcan.jpg", "hithere.jpg", "cutebaby.jpg"]
   # DPhoto = ["boss-bb.jpg"]
 
   # has_attached_file :image, default_url: "boss-bb.jpg"
-  has_attached_file :image, default_url: DPhoto.shuffle.first
+  has_attached_file :image, default_url: Default.shuffle.first
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   belongs_to :author,
