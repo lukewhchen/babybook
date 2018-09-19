111b025e75b5f028bd569a7ef2b394d8d0a96a6f
lukewhchen <lukewhchen@gmail.com>
2018-09-19 16:39:07 -0400

    add comment reducer

--- frontend/reducers/comments_reducer.js
+++ frontend/reducers/comments_reducer.js
@@ -1,6 +1,22 @@
-// const commentsReducer = (state = {}, action) => {
-//
-// };
-//
-//
-// export default commentsReducer;
+import merge from 'lodash/merge';
+import { RECEIVE_POSTS, RECEIVE_FEED, RECEIVE_POST } from '../actions/post_actions';
+import { RECEIVE_COMMENT, REMOVE_COMMENT } from '../actions/comments_actions';
+
+
+const commentsReducer = (state = {}, action) => {
+
+  switch (action.type) {
+    case RECEIVE_COMMENT: {
+      return merge({}, state, {[action.comment.id]: action.comment});
+    }
+    case REMOVE_COMMENT: {
+      const newState = merge({}, state);
+      delete newState[action.comment.id];
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+export default commentsReducer;
