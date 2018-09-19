801b499db64258099358d191fde934ab723e401a
lukewhchen <lukewhchen@gmail.com>
2018-09-19 15:16:04 -0400

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,10 +30,11 @@ class User < ApplicationRecord
   has_many :posts,
     foreign_key: :author_id,
     class_name: :Post
-    
+
   has_many :comments,
     foreign_key: :author_id,
-    class_name: :Comment
+    class_name: :Comment,
+    dependent: :destroy
 
   attr_reader :password
 
