79dd0d980a3943cab064a007ebd944fb421c8644
lukewhchen <lukewhchen@gmail.com>
2018-11-09 21:57:03 -0800

    change font

--- frontend/components/post/left_sidebar.jsx
+++ frontend/components/post/left_sidebar.jsx
@@ -6,19 +6,19 @@ const LeftSidebar = ({ currentUser }) => {
     <li className="left-side">
       <label className="left-icon">
         <Link to={`/users/${currentUser.id}`}>
-        <i className="fa fa-user-circle" aria-hidden="true">&nbsp;&nbsp;{currentUser.first_name}</i><br/></Link>
+        <i className="fa fa-user-circle" aria-hidden="true"><ul>&nbsp;&nbsp;{currentUser.first_name}</ul></i><br/></Link>
         <a href='https://www.independent.co.uk/topic/babies' target='_blank'>
-          <i className="fa fa-newspaper-o" aria-hidden="true">&nbsp;&nbsp;More Babies</i><br/></a>
+          <i className="fa fa-newspaper-o" aria-hidden="true"><ul>&nbsp;&nbsp;More Babies</ul></i><br/></a>
         <a href='https://lukewhchen.github.io/Flip/' target='_blank'>
-          <i className="fa fa-gamepad" aria-hidden="true">&nbsp;&nbsp;Puzzle Game</i><br/></a>
+          <i className="fa fa-gamepad" aria-hidden="true"><ul>&nbsp;&nbsp;Puzzle Game</ul></i><br/></a>
         <br/>
         <p className='underline'>About Babybook</p>
         <a href='https://www.linkedin.com/in/luke-chen-42342458/' target='_blank'>
-          <i className="fa fa-linkedin-square" aria-hidden="true">&nbsp;&nbsp;LinkedIn</i></a>
+          <i className="fa fa-linkedin-square" aria-hidden="true"><ul>&nbsp;&nbsp;LinkedIn</ul></i></a>
         <a href='https://github.com/lukewhchen' target='_blank'>
-          <i className="fa fa-github" aria-hidden="true">&nbsp;&nbsp;Github</i></a>
+          <i className="fa fa-github" aria-hidden="true"><ul>&nbsp;&nbsp;Github</ul></i></a>
         <a href='https://lukewhchen.github.io/' target='_blank'>
-          <i className="fa fa-tv" aria-hidden="true">&nbsp;Portfolio</i><br/></a>
+          <i className="fa fa-tv" aria-hidden="true"><ul>&nbsp;Portfolio</ul></i><br/></a>
       </label>
     </li>
   );
