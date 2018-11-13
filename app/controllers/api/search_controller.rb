5ea7718eb393fa790850f86c94ba33c578d7e9af
lukewhchen <lukewhchen@gmail.com>
2018-11-13 15:32:42 -0800

    create search contoller

---
+++ app/controllers/api/search_controller.rb
@@ -0,0 +1,6 @@
+class Api::SearchController < ApplicationController
+  def search
+    @users = User.where("LOWER(first_name) LIKE :input OR LOWER(last_name) LIKE :input", input: "%#{params[:input].downcase}%")
+    render 'api/users/index'
+  end
+end
