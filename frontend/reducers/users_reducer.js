31c0870740e2231dabd2829c8fe598e235e639ff
lukewhchen <lukewhchen@gmail.com>
2018-11-13 16:05:46 -0800

    add user reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -2,10 +2,12 @@ import merge from 'lodash/merge';
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 import { RECEIVE_USERS, RECEIVE_USER } from '../actions/user_actions';
 import { RECEIVE_POSTS, RECEIVE_POST} from '../actions/post_actions';
+import { RECEIVE_SEARCH_RESULTS } from '../actions/search_actions';
 
 
 const usersReducer =  (state = {}, action) => {
   Object.freeze(state);
+  let newState = merge({}, state);
 
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
@@ -18,6 +20,12 @@ const usersReducer =  (state = {}, action) => {
       return merge({}, state, action.authors);
     case RECEIVE_POST:
       return merge({}, state, {[action.author.id]: action.author});
+    case RECEIVE_SEARCH_RESULTS:
+      Object.values(action.users).forEach(id => {
+        delete newState[id];
+        newState[id] = action.users[id];
+      });
+      return newState;
     default:
       return state;
   }
