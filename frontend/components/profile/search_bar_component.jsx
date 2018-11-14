d65ead85a2aec328695cf8eb8d4eace51d81d7e2
lukewhchen <lukewhchen@gmail.com>
2018-11-13 16:39:30 -0800

    add search component

---
+++ frontend/components/profile/search_bar_component.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
+
+class SearchBarComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.clearInput = this.clearInput.bind(this);
+    this.state = {
+      input: '',
+      searchResults: this.props.searchResults
+    };
+  }
+
+  handleSearch(e) {
+    e.preventDefault();
+    this.setState({ input: e.target.value }, () => {
+      if (this.state.input.length > 0) {
+        this.props.fetchSearchResults(this.state.input);
+      }
+    });
+  }
+
+  componentWillUpdate(nextProps) {
+    if (this.state.searchResults.length !== nextProps.searchResults.length) {
+      this.setState({ searchResults: nextProps.searchResults });
+    }
+
+    if (this.props.location.pathname !== nextProps.location.pathname) {
+      this.props.clearSearchResults();
+      this.clearInput();
+    }
+  }
+
+  clearInput() {
+    this.setState({ input: '' });
+  }
+
+  render() {
+    let searchResults;
+    if (this.state.searchResults.length < 1) {
+      searchResults = null;
+    } else {
+      const searchResultItems = this.state.searchResults.map(user => {
+        return(
+          <li className='search-result' key={user.id}>
+            <Link to={`/users/${user.id}`} className='search-result-content'>
+              <div className='main-nav-minipic'>
+                <img src={user.profilePicUrl} />
+              </div>
+              <p>{user.fname} {user.lname}</p>
+            </Link>
+          </li>
+        );
+      });
+
+      searchResults = (
+        <ul className='search-results content-item'>
+          {searchResultItems}
+        </ul>
+      );
+    }
+
+    return (
+      <div>
+        <div className='nav-search-wrapper'>
+          <input
+            onChange={this.handleSearch}
+            type='text'
+            placeholder='Search'
+            value={this.state.input}
+          />
+          <div className='nav-search-icon'>
+            <img src={window.magnifierURL} />
+          </div>
+        </div>
+        {searchResults}
+      </div>
+    );
+  }
+}
+
+export default withRouter(SearchBarComponent);
