fb21abf30773c69efe42cc0ddd07a7a6c84f66c9
lukewhchen <lukewhchen@gmail.com>
2019-07-25 16:45:11 -0700

    makeup foot info

--- frontend/components/welcome/footer_message.js
+++ frontend/components/welcome/footer_message.js
@@ -3,7 +3,15 @@ import React from 'react';
 const FooterMessage = () => {
   return (
     <div className='welcome-foot'>
-      <p>Babybook @ 2018 developed by Luke Chen</p>
+      <div className="foot-info">
+        <p>English (US)</p>
+        <hr></hr>
+        <p>Babybook by Luke Chen 2019. Connect with me on
+          <a href='https://www.linkedin.com/in/luke-chen-42342458/' target='_blank'> LinkedIn </a>
+           or
+          <a href='https://github.com/lukewhchen' target='_blank'> Github</a>.
+        </p>
+    </div>
     </div>
   );
 };
