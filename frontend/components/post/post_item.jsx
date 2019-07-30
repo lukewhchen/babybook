cce172aa2f117648ba22f6a66780aa3e0c17a5fa
lukewhchen <lukewhchen@gmail.com>
2019-07-29 23:01:32 -0700

    refactoring single post

--- frontend/components/post/post_item.jsx
+++ frontend/components/post/post_item.jsx
@@ -4,22 +4,39 @@ import { Link, withRouter } from "react-router-dom";
 
 
 function PostItem(props) {
+  const timestamp = new Intl.DateTimeFormat('en-US', {
+      month: 'long',
+      day: '2-digit',
+      hour: 'numeric',
+      minute: 'numeric',
+    }).format(new Date(props.post.created_at));
   const author = props.post.author_name;
   const authorURL = `/users/${props.post.author_id}`;
   const postContent = props.post.body;
   const postImage = props.post.image_url;
   return (
-    <li className="single-post">
-      <Link className="user" to={authorURL}><i className="fa fa-user-circle" aria-hidden="true"/>{author}</Link>
-      <ul>{postContent}</ul>
+    <div className="single-post">
+      <div className="post-header">
+        <div className="avatar">
+          <Link to={authorURL}><i className="fa fa-user-circle" aria-hidden="true"/></Link>
+        </div>
+        <div className="post-info">
+          <ul><Link to={authorURL}>{author}</Link></ul>
+          <ul>{timestamp}</ul>
+        </div>
+      </div>
+      <div className="post-content">
+        <ul>{postContent}</ul>
         <img src={postImage}/>
-      <br/>
-      <label className="like-comment-share">
-        <i className="fa fa-thumbs-o-up" aria-hidden="true">Like</i>
-        <i className="fa fa-commenting-o" aria-hidden="true">Comment</i>
-        <i className="fa fa-share" aria-hidden="true">Share</i>
-      </label>
-    </li>
+      </div>
+      <div className="post-footer">
+        <label className="like-comment-share">
+          <i className="fa fa-thumbs-o-up" aria-hidden="true">Like</i>
+          <i className="fa fa-commenting-o" aria-hidden="true">Comment</i>
+          <i className="fa fa-share" aria-hidden="true">Share</i>
+        </label>
+      </div>
+    </div>
   );
 }
 
