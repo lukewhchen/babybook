b2267d09e8d34cc7488ab162444fc5790636188d
lukewhchen <lukewhchen@gmail.com>
2019-09-03 15:11:48 -0700

    start add weather card

---
+++ frontend/components/feed/advertise.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Advertise = () => {
+  return (
+    <div>
+      <li>
+        <h4>Best Things to Do With Kids in the Bay Area This Weekend</h4>
+        <i className="fa fa-child" aria-hidden="true" />
+          <a href="http://www.marinemammalcenter.org/" target="_blank">Meet the marine mammals in Sausalito</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+          <a href="https://www.mlb.com/giants/ballpark/attractions" target="_blank">Root for the home team</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+          <a href="https://goldengatepark.com/" target="_blank">Spend some time in Golden Gate Park</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+          <a href="http://www.ebparks.org/parks/tilden/" target="_blank">Take the tots to Tilden</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+          <a href="https://www.cityofberkeley.info/adventureplayground/" target="_blank">Up your adventure in Berkeley</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+          <a href="https://www.spn.usace.army.mil/missions/recreation/bay-model-visitor-center/" target="_blank">Explore the Bay Model</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+          <a href="http://redtri.com/san-francisco/things-to-do-in-the-presidio/slide/1" target="_blank">Play in the Presidio</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+          <a href="http://redtri.com/san-francisco/best-san-francisco-playgrounds/" target="_blank">Check out a new playground</a><br/>
+        <i className="fa fa-child" aria-hidden="true" />
+          <a href="https://www.independent.co.uk/news/business/news/peppa-pig-entertainment-one-117-new-episodes-cartoon-global-market-a7743991.html" target="_blank">Peppa Pig announces new episodes</a><br/>
+      </li>
+    </div>
+  );
+};
+
+export default Advertise;
